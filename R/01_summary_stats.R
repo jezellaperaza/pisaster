c4a4b8fd7d1b1edbe2ee28d98218923369dcf4ef
Jezella Peraza <jezper@uw.edu>
2023-01-29 10:05:15 -0800

    updated summary_stats R script

--- R/01_summary_stats.R
+++ R/01_summary_stats.R
@@ -14,3 +14,12 @@ head(pisaster_data)
 
 ## calculate mean counts across all years, sites, and plots
 mean_count <- mean(pisaster_data$count)
+
+## write the first 10 rows of data
+dput(pisaster_data[1:10,])
+
+dat <- structure(list(2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 
+                      2019, 2019, "a", "a", "a", "a", "a", "b", "b", "b", "b", 
+                      "b", 1L, 2L, 3L, 4L, 5L, 1L, 2L, 3L, 4L, 5L, 6L, 10L, 13L, 
+                      9L, 9L, 11L, 7L, 10L, 12L, 8L), .Dim = c(10L, 4L), .Dimnames = list(
+                        NULL, c("year", "site", "plot", "count")))
